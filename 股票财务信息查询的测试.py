7171f19504a2c71b9904616e71d3393b49b86f5b
louisgogo <louse12345@163.com>
2017-03-20 01:14:30 +0800

    改进

---
+++ 股票财务信息查询的测试.py
@@ -0,0 +1,22 @@
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import datetime
+import requests
+
+url = 'http://www.cninfo.com.cn/information/stock/financialreport_.jsp?stockCode=000002'
+# 可选参数包括：incomestatements，balancesheet，cashflow，financialreport，注意变更url和data中对应的参数
+headers = {
+    'Host': 'www.cninfo.com.cn',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'Accept': 'text/css,*/*;q=0.1',
+    'Connection': 'keep-alive'
+}
+data = {'yyyy': '2014',
+        'mm': '-12-31',
+        'cwzb': 'financialreport'
+        }
+html = requests.post(url, headers=headers, allow_redirects=False, data=data)
+print(html.encoding)
+html.encoding = 'gbk'
+print(html.text)
